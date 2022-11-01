6bd71f25d4c65043c336e252144461bd87cd5378
Alan Hesu <alanhesu@gmail.com>
2022-11-01 17:36:30 -0400

    try to use convlstm

--- roboturk_loader.py
+++ roboturk_loader.py
@@ -9,6 +9,7 @@ import torchvision.transforms as transforms
 import argparse
 import cv2
 import os
+import glob
 
 class RoboTurk(data.Dataset):
     def __init__(self, num_frames=5, stride=1, dir='/media/jer/data/bouncing_ball_1000_1/test1_bouncing_ball', stage='raw', shuffle=True):
@@ -18,18 +19,22 @@ class RoboTurk(data.Dataset):
         self.stride = stride
         self.indices, self.dataset = self.get_data(shuffle=shuffle)
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model = Transformer()       
+        model = Transformer()
         self.SOS_token = torch.ones((1, model.dim_model), dtype=torch.float32, device=device) * 2
         # self.EOS_token = torch.ones((1, model.dim_model), dtype=torch.float32) * 3
 
     def __getitem__(self, index):
         # obtaining file paths
-        frame_names = self.dataset[index]
+        frame_names = self.dataset[index][0]
+
+        jointdata = np.load(self.dataset[index][1])
+        jointdata = torch.from_numpy(jointdata).float()
 
         # loading and formatting image
         frames=[]
         for frame_name in frame_names:
             frame = cv2.imread(frame_name)
+            frame = cv2.resize(frame, (128, 128))
             # frame = self.transform(frame) # TODO: add transforms
 
         #     # check decoding
@@ -37,27 +42,27 @@ class RoboTurk(data.Dataset):
         #     # reconstruction = np.array(reconstruction[0])
         #     # cv2.imshow('reconstruction', reconstruction)
         #     # cv2.waitKey(0)
-            
-            frame = frame.squeeze(0)
-        #     # frame = torch.from_numpy(frame)
-        #     # frame = frame.permute(2, 0, 1)
-        #     # frame = frame.float()/255.0
-            
+
+            # frame = frame.squeeze(0)
+            frame = torch.from_numpy(frame)
+            frame = frame.permute(2, 0, 1)
+            frame = frame.float()/255.0
+
         #     # bs, seq_len, _, _, _ = frame.shape
-            frame = frame.flatten()
-                
+            # frame = frame.flatten()
+
             frames.append(frame)
-        
+
         # # creating tensor with requires_grad=False
         frames = torch.stack(frames, dim=0)
         frames = frames.detach()
         frames.requires_grad = False
-        
-        # # concatenating SOS token, 
-        frames = torch.cat((self.SOS_token, frames), dim=0)
-                    
-        #  frames.shape: (seq_len + 1, dim_model)   
-        return self.indices[index], frames
+
+        # # concatenating SOS token,
+        # frames = torch.cat((self.SOS_token, frames), dim=0)
+
+        #  frames.shape: (seq_len + 1, dim_model)
+        return {'data':frames, 'y':jointdata}
 
     def __len__(self):
         return len(self.dataset)
@@ -69,58 +74,52 @@ class RoboTurk(data.Dataset):
         indices = []
 
         # crawling the directory
-        for dir, _, files in os.walk(self.dir):
-            for file in files:
-                # saving (time, path)
-                parent = dir.split('/')[-1]
-                # (parent+index, name)
-                if file.endswith('.jpg'):
-                    parent_index = parent.split('_')[-1]
-                    # img_names.append((int(parent+file[-7:-4]), os.path.join(dir, file)))
-                    img_names.append((int(parent_index+file[-7:-4]), os.path.join(dir, file)))
-                if file.endswith('.npy'):
-                    parent_index = parent.split('_')[-2]
-                    joint_data.append((int(parent_index+file[-7:-4]), os.path.join(dir, file)))
-                    
+        for dir in glob.glob(os.path.join(self.dir, '*/'), recursive=True):
+            parent = os.path.split(os.path.split(dir)[0])[1]
+            for file in glob.glob(os.path.join(dir, '*.jpg')):
+                parent_index = parent.split('_')[-1]
+                img_names.append((int(parent_index+file[-7:-4]), os.path.join(dir, file)))
+            for file in glob.glob(os.path.join(dir, '*.npy')):
+                parent_index = parent.split('_')[-2]
+                joint_data.append((int(parent_index+file[-7:-4]), os.path.join(dir, file)))
+
         # sorting the names numerically. first 4 digits are folder and last 3 are file
         img_names = sorted(img_names, key=lambda x: x[0])
+        joint_data = sorted(joint_data, key=lambda x: x[0])
 
         # indices = [x[0] for x in img_names]
 
-        for i in range(0, len(img_names), self.num_frames):
+        for i in range(0, len(img_names)-self.num_frames, self.num_frames):
             index_list = []
             frame_names = []
             for j in range(0, self.num_frames, self.stride):
-                if i+j < len(img_names):
-                    index_list.append(img_names[i+j][0])
-                    frame_names.append(img_names[i+j][1])
-                else:
-                    break
-            
-            # list of lists of frame indices 
-            indices.append(index_list) 
-                
-            # each element is a list of frame names with length num_frames and skipping frames according to stride    
-            dataset.append(frame_names, joint_data[i][1])
-            
+                index_list.append(img_names[i+j][0])
+                frame_names.append(img_names[i+j][1])
+
+            # list of lists of frame indices
+            indices.append(index_list)
+
+            # each element is a list of frame names with length num_frames and skipping frames according to stride
+            dataset.append((frame_names, joint_data[i+self.num_frames][1]))
+
         if shuffle:
             np.random.shuffle(dataset)
         else:
             dataset = np.array(dataset)
-        
+
         return indices, dataset
-        
+
 
 if __name__ == '__main__':
-    dataset = RoboTurk(num_frames=5, stride=1, dir='/media/jer/data/bouncing_ball_1000_1/test1_bouncing_ball', stage='train', shuffle=True)
-    
+    dataset = RoboTurk(num_frames=5, stride=1, dir='/home/alanhesu/Documents/github/vroom/RoboTurk_videos/bins-Bread', stage='train', shuffle=True)
+
     for i in range(10):
         print('dir: ', dataset.dir)
         print('clip ', i)
         print("clips in the dataset: ", len(dataset.dataset))
-        print('clip length: ', len(dataset[0]))
-        print('frame shape: ', dataset[0][0].shape)
-        frames = dataset[i]
+        # print('clip length: ', len(dataset[0]))
+        print('frame shape: ', dataset[0]['data'].shape)
+        frames = dataset[i]['data']
         for frame in frames:
             print(frame.size())
             frame = frame.permute(1, 2, 0)
---
+++ trainer_lstm.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, RandomSampler
+import math
+import numpy as np
+from Seq2Vec import Seq2Vec
+import torchvision.transforms as transforms
+import argparse
+import os
+from tqdm import tqdm
+import cv2
+from PIL import Image, ImageDraw
+
+from roboturk_loader import RoboTurk
+
+class Trainer():
+    def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print('device: ', self.device)
+        self.model = Seq2Vec(num_channels=3, num_kernels=64, kernel_size=(3,3), padding=(1,1), activation="relu", frame_size=(128,128), num_layers=3).to(self.device)
+
+    def train_loop(self, model, opt, loss_fn, dataloader, frames_to_predict): # TODO: move encoding from dataloader to here
+        model = model.to(self.device)
+        model.train()
+        total_loss = 0
+
+        for i, batch in enumerate(tqdm(dataloader)):
+            X = batch['data']
+            y_actual = batch['y']
+
+            X = X.permute(0, 2, 1, 3, 4)
+            X = X.clone().detach().requires_grad_(True).to(self.device)
+            # X = torch.tensor(X).to(self.device)
+            y_actual = y_actual.clone().detach().to(self.device)
+
+            pred = model(X)
+
+            loss = loss_fn(y_actual, pred)
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            total_loss += loss.detach().item()
+
+        return total_loss / len(dataloader)
+
+    def validation_loop(self, model, loss_fn, dataloader, frames_to_predict):
+        model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for j, (index_list, batch) in enumerate(tqdm(dataloader)):
+                X = batch['data']
+                y_actual = batch['y']
+
+                X = X.permute(0, 2, 1, 3, 4)
+                X = X.clone().detach().to(self.device)
+                y_actual = y_actual.clone().detach().to(self.device)
+
+                pred = model(X)
+
+                loss = loss_fn(y_actual, pred)
+
+                total_loss += loss.detach().item()
+
+        return total_loss / len(dataloader)
+
+    def fit(self, model, opt, loss_fn, train_dataloader, val_dataloader, epochs, frames_to_predict):
+        # Used for plotting later on
+        train_loss_list, validation_loss_list = [], []
+
+        print("Training and validating model")
+        for epoch in range(epochs):
+            if epochs > 1:
+                print("-"*25, f"Epoch {epoch + 1}","-"*25)
+
+            train_loss = self.train_loop(model, opt, loss_fn, train_dataloader, frames_to_predict)
+            train_loss_list += [train_loss]
+
+            validation_loss = self.validation_loop(model, loss_fn, val_dataloader, frames_to_predict)
+            validation_loss_list += [validation_loss]
+
+            print(f"Training loss: {train_loss:.4f}")
+            print(f"Validation loss: {validation_loss:.4f}")
+
+        # counting number of files in ./checkpoints
+        index = len(os.listdir('./checkpoints'))
+
+        if epochs > 1:
+            # save model
+            torch.save(model.state_dict(), './checkpoints/model' + '_' + str(index) + '.pt')
+            print('model saved as model' + '_' + str(index) + '.pt')
+
+        return train_loss_list, validation_loss_list
+
+    def custom_collate(self, batch):
+        filtered_batch = []
+        for video, _, label in batch:
+            filtered_batch.append((video, label))
+        return torch.utils.data.dataloader.default_collate(filtered_batch)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--save_best', type=bool, default=False)
+    parser.add_argument('--folder', type=str, required=True)
+    parser.add_argument('--name', type=str, required=True)
+    args = parser.parse_args()
+
+    # torch.multiprocessing.set_start_method('spawn')
+
+    frames_per_clip = 5
+    frames_to_predict = 5
+    stride = 1 # number of frames to shift when loading clips
+    batch_size = 32
+    epoch_ratio = 1 # to sample just a portion of the dataset
+    epochs = 10
+    lr = 0.00001
+    num_workers = 0
+
+    dim_model = 256
+    num_heads = 8
+    num_encoder_layers = 6
+    num_decoder_layers = 6
+    dropout_p = 0.1
+
+    trainer = Trainer()
+
+    model = trainer.model
+    opt = optim.Adam(model.parameters(), lr=lr)
+    loss_fn = nn.MSELoss() # TODO: change this to mse + condition + gradient difference
+    if args.dataset == 'roboturk':
+        train_dataset = RoboTurk(num_frames=5, stride=stride, dir=args.folder, stage='train', shuffle=True)
+        train_sampler = RandomSampler(train_dataset, replacement=False, num_samples=int(len(train_dataset) * epoch_ratio))
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False, sampler=train_sampler, num_workers=num_workers)
+
+        test_dataset = RoboTurk(num_frames=5, stride=stride, dir=args.folder, stage='test', shuffle=True)
+        test_sampler = RandomSampler(test_dataset, replacement=False, num_samples=int(len(test_dataset) * epoch_ratio))
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, sampler=test_sampler, num_workers=num_workers)
+
+    if args.save_best:
+        best_loss = 1e10
+        epoch = 1
+        while True:
+            print("-"*25, f"Epoch {epoch}","-"*25)
+            train_loss_list, validation_loss_list = trainer.fit(model=model, opt=opt, loss_fn=loss_fn, train_dataloader=train_loader, val_dataloader=test_loader, epochs=1, frames_to_predict=frames_to_predict)
+            if validation_loss_list[-1] < best_loss:
+                best_loss = validation_loss_list[-1]
+                torch.save(model.state_dict(), './checkpoints/model_' + args.name + '.pt')
+                print('model saved as model_' + str(args.name) + '.pt')
+            epoch += 1
+    else:
+        trainer.fit(model=model, opt=opt, loss_fn=loss_fn, train_dataloader=train_loader, val_dataloader=test_loader, epochs=epochs, frames_to_predict=frames_to_predict)
