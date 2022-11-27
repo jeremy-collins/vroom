a72c8819981a52466e15a7965224261ae2454852
Alan Hesu <alanhesu@gmail.com>
2022-11-26 21:55:46 -0500

    attempt at replicating imitation bc

---
+++ bc_mlp.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.distributions
+from functools import partial
+import numpy as np
+
+class BC_MLP(nn.Module):
+    def __init__(self, input_size, output_size, net_arch):
+        super().__init__()
+        self.input_size = input_size
+        self.net_arch = net_arch
+        self.output_size = output_size
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        layers = []
+        prev_layer_size = input_size
+        for layer in net_arch:
+            layers.append(nn.Linear(prev_layer_size, layer))
+            layers.append(nn.Tanh())
+            prev_layer_size = layer
+
+        self.layers = nn.Sequential(*layers).to(self.device)
+
+    def forward(self, X):
+        return self.layers(X)
+
+class BC_custom(nn.Module):
+    def __init__(self, input_size, output_size, net_arch, log_std_init=0, deterministic=False, ortho_init=True):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.net_arch = net_arch
+        self.deterministic = deterministic
+        self.ortho_init = ortho_init
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.extract_features = nn.Flatten() # this can be a CNN for images
+
+        self.action_net = nn.Linear(net_arch[-1], self.output_size)
+        self.value_net = nn.Linear(net_arch[-1], 1)
+
+        self.mlp = BC_MLP(input_size, output_size, net_arch)
+
+        self.mean_actions = nn.Linear(net_arch[-1], output_size)
+        self.log_std = nn.Parameter(torch.ones(output_size)*log_std_init, requires_grad=True)
+
+        # initialization
+        if (self.ortho_init):
+            module_gains = {
+                self.extract_features: np.sqrt(2),
+                self.mlp: np.sqrt(2),
+                self.action_net: 0.01,
+                self.value_net: 1,
+            }
+
+            for module, gain in module_gains.items():
+                module.apply(partial(self.init_weights gain=gain))
+
+    def forward(self, X):
+        features = self.extract_features(X)
+        latent = self.mlp(features)
+        values = self.value_net(latent)
+        mean_actions = self.action_net(latent)
+        self.proba_distribution(mean_actions, self.log_std) # self._get_action_dist_from_latent(latent_pi)
+        if (self.determinstic):
+            # mode
+            actions = self.distribution.mean()
+        else:
+            # random sample
+            actions = self.distribution.rsample()
+        log_prob = self.sum_independent_dims(self.distribution.log_prob(actions))
+        actions = actions.reshape((-1,) + self.output_size)
+        return actions, values, log_prob
+
+    def proba_distribution(self, mean_actions, log_std):
+        action_std = torch.ones_like(mean_actions)*log_std.exp()
+        self.distribution = distributions.Normal(mean_actions, action_std)
+
+    def evaluate_actions(self, obs, actions):
+        features = self.extract_features(obs)
+        latent = self.mlp(features)
+        mean_actions = self.action_net(latent)
+        self.proba_distribution(mean_actions, self.log_std) # self._get_action_dist_from_latent(latent_pi)
+        log_prob = self.sum_independent_dims(self.distribution.log_prob(actions))
+        values = self.value_net(latent)
+        entropy = self.sum_independent_dims(self.distribution.entropy())
+        return values, log_prob, entropy
+
+    def sum_independent_dims(tensor):
+        """
+        Continuous actions are usually considered to be independent,
+        so we can sum components of the ``log_prob`` or the entropy.
+
+        :param tensor: shape: (n_batch, n_actions) or (n_batch,)
+        :return: shape: (n_batch,)
+        """
+        if len(tensor.shape) > 1:
+            tensor = tensor.sum(dim=1)
+        else:
+            tensor = tensor.sum()
+        return tensor
+
+    @staticmethod
+    def init_weights(module: nn.Module, gain: float = 1) -> None:
+        """
+        Orthogonal initialization (used in PPO and A2C)
+        """
+        if isinstance(module, (nn.Linear, nn.Conv2d)):
+            nn.init.orthogonal_(module.weight, gain=gain)
+            if module.bias is not None:
+                module.bias.data.fill_(0.0)
---
+++ trainer_bc.py
@@ -0,0 +1,201 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, RandomSampler
+import math
+import numpy as np
+from bc_mlp import BC_custom
+import torchvision.transforms as transforms
+import argparse
+import os
+from tqdm import tqdm
+import cv2
+from PIL import Image, ImageDraw
+from utils import Utils
+
+from roboturk_loader_observations import RoboTurkObs
+
+class TrainerBC():
+    def __init__(self, ent_weight=0, l2_weight=0):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print('device: ', self.device)
+        self.ent_weight = ent_weight
+        self.l2_weight = l2_weight
+        # self.utils = Utils()
+        # self.utils.init_resnet()
+        # self.utils.init_resnet(freeze=False)
+
+    # def encode_img(self, img):
+    #     # input image into CNN
+    #     # img = np.array(img, dtype=np.float32)
+    #     # img = cv2.resize(img, (224, 224))
+    #     latents = self.resnet50(img)
+    #     # img = torch.tensor(latents).to(self.device)
+    #     return latents
+
+
+    def train_loop(self, model, opt, dataloader):
+        model = model.to(self.device)
+        model.train()
+        total_loss = 0
+
+        for i, batch in enumerate(tqdm(dataloader)):
+            X = batch['data']
+            X = torch.tensor(X).to(self.device)
+            # X = X.clone().detach().to(self.device)
+
+            y = batch['y']
+            # y = y.clone().detach().to(self.device)?
+            y = torch.tensor(y).to(self.device)
+
+            _, log_prob, entropy = model.evaluate_actions(X, y)
+
+            y_expected = batch['y']
+            y_expected = torch.tensor(y_expected).to(self.device)
+
+            prob_true_act = torch.exp(log_prob).mean()
+            log_prob = log_prob.mean()
+            entropy = entropy.mean()
+
+            l2_norms = [torch.sum(torch.square(w)) for w in model.parameters()]
+            l2_norm = sum(l2_norms) / 2  # divide by 2 to cancel with gradient of square
+            # sum of list defaults to float(0) if len == 0.
+            assert isinstance(l2_norm, torch.Tensor)
+
+            ent_loss = -self.ent_weight * entropy
+            neglogp = -log_prob
+            l2_loss = self.l2_weight * l2_norm
+            loss = neglogp + ent_loss + l2_loss
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            total_loss += loss.detach().item()
+
+        return total_loss / len(dataloader)
+
+    def validation_loop(self, model, dataloader):
+        model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for i, batch in enumerate(tqdm(dataloader)):
+                X = batch['data']
+                X = torch.tensor(X).to(self.device)
+                # X = X.clone().detach().to(self.device)
+
+                y = batch['y']
+                # y = y.clone().detach().to(self.device)?
+                y = torch.tensor(y).to(self.device)
+
+                _, log_prob, entropy = model.evaluate_actions(X, y)
+
+                y_expected = batch['y']
+                y_expected = torch.tensor(y_expected).to(self.device)
+
+                prob_true_act = torch.exp(log_prob).mean()
+                log_prob = log_prob.mean()
+                entropy = entropy.mean()
+
+                l2_norms = [torch.sum(torch.square(w)) for w in model.parameters()]
+                l2_norm = sum(l2_norms) / 2  # divide by 2 to cancel with gradient of square
+                # sum of list defaults to float(0) if len == 0.
+                assert isinstance(l2_norm, torch.Tensor)
+
+                ent_loss = -self.ent_weight * entropy
+                neglogp = -log_prob
+                l2_loss = self.l2_weight * l2_norm
+                loss = neglogp + ent_loss + l2_loss
+
+                total_loss += loss.detach().item()
+
+        return total_loss / len(dataloader)
+
+    def fit(self, model, opt, train_dataloader, val_dataloader, epochs):
+        # Used for plotting later on
+        train_loss_list, validation_loss_list = [], []
+
+        print("Training and validating model")
+        for epoch in range(epochs):
+            if epochs > 1:
+                print("-"*25, f"Epoch {epoch + 1}","-"*25)
+
+            train_loss = self.train_loop(model, opt, train_dataloader)
+            train_loss_list += [train_loss]
+
+            validation_loss = self.validation_loop(model, val_dataloader)
+            validation_loss_list += [validation_loss]
+
+            print(f"Training loss: {train_loss:.4f}")
+            print(f"Validation loss: {validation_loss:.4f}")
+
+        # counting number of files in ./checkpoints
+        index = len(os.listdir('./checkpoints'))
+
+        if epochs > 1:
+            # save model
+            torch.save(model.state_dict(), './checkpoints/model' + '_' + str(index) + '.pt')
+            print('model saved as model' + '_' + str(index) + '.pt')
+
+        return train_loss_list, validation_loss_list
+
+    def custom_collate(self, batch):
+        filtered_batch = []
+        for video, _, label in batch:
+            filtered_batch.append((video, label))
+        return torch.utils.data.dataloader.default_collate(filtered_batch)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--save_best', type=bool, default=False) # only save best model
+    parser.add_argument('--folder', type=str, required=True) # dataset location
+    parser.add_argument('--name', type=str, required=True) # name of the model
+
+    args = parser.parse_args()
+
+    # torch.multiprocessing.set_start_method('spawn')
+
+    frames_per_clip = 1
+    frames_to_predict = 1 # must be <= frames_per_clip
+    stride = 1 # number of frames to shift when loading clips
+    batch_size = 32
+    epoch_ratio = 1 # to sample just a portion of the dataset
+    epochs = 50
+    lr = 1e-4
+    num_workers = 6
+
+    dim_model = 2048
+    num_heads = 8
+    num_encoder_layers = 4
+    num_decoder_layers = 4
+    dropout_p = 0
+
+    trainer = TrainerBC()
+
+    model = BC_custom(input_size=26, output_size=4, net_arch=[32,32])
+    opt = optim.Adam(model.parameters(), lr=lr)
+    # loss_fn = nn.MSELoss() # TODO: change this to mse + condition + gradient difference
+    if args.dataset == 'roboturk':
+        train_dataset = RoboTurkObs(num_frames=frames_per_clip, stride=stride, dir=args.folder, stage='train', shuffle=True)
+        train_sampler = RandomSampler(train_dataset, replacement=False, num_samples=int(len(train_dataset) * epoch_ratio))
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False, sampler=train_sampler, num_workers=num_workers)
+
+        test_dataset = RoboTurkObs(num_frames=frames_per_clip, stride=stride, dir=args.folder, stage='test', shuffle=True)
+        test_sampler = RandomSampler(test_dataset, replacement=False, num_samples=int(len(test_dataset) * epoch_ratio))
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, sampler=test_sampler, num_workers=num_workers)
+
+    if args.save_best:
+        best_loss = 1e10
+        epoch = 1
+        while True:
+            print("-"*25, f"Epoch {epoch}","-"*25)
+            train_loss_list, validation_loss_list = trainer.fit(model=model, opt=opt, train_dataloader=train_loader, val_dataloader=test_loader, epochs=1)
+            if validation_loss_list[-1] < best_loss:
+                best_loss = validation_loss_list[-1]
+                torch.save(model.state_dict(), './checkpoints/model_' + args.name + '.pt')
+                print('model saved as model_' + str(args.name) + '.pt')
+            epoch += 1
+    else:
+        trainer.fit(model=model, opt=opt, train_dataloader=train_loader, val_dataloader=test_loader, epochs=epochs)
